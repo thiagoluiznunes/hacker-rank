36768d17ea9d845316afb00fd6367918a8b68d25
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-12-20 23:10:04 -0300

    init 10days of javascript

---
+++ 10Days-of-Javascript/day00:hello-world/tutorial/stdin-stdout.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const questions = [
+    'What is your name?',
+    'What is your fav habboy?',
+    'What is your preferred programing language?'
+];
+
+const answers = [];
+
+function ask(i) {
+	process.stdout.write(`\n${questions[i]} \n\n`);
+}
+
+process.stdin.on('data', (data) => {
+	answers.push(data.toString().trim());
+
+	if (answers.length < questions.length) {
+		ask(answers.length);
+	} else {
+		process.exit();
+	}
+});
+
+ask(0);
