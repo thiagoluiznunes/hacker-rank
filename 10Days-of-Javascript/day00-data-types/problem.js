7747b44cb2d69793c2495e33ef0056ac4a9e48f0
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-12-23 02:50:29 -0300

    add solution to data-types challenge

--- 10Days-of-Javascript/day00-data-types/problem.js
+++ 10Days-of-Javascript/day00-data-types/problem.js
@@ -1,1 +1,33 @@
 'use strict'
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const firstInteger = 4;
+const firstDecimal = 4.0;
+const firstString = 'HackerRank ';
+
+rl.on('line', input => {
+    const array = input.split(' ');
+    
+    const secondInteger = parseInt(array[0]);
+    const secondDecimal = parseFloat(array[1]);
+    const secondString = array[2];
+    
+    console.log(`\n${secondInteger} ${typeof secondInteger}`);
+    console.log(`${secondDecimal} ${typeof secondDecimal}`);
+    console.log(`${secondString} ${typeof secondString}\n`);
+
+    console.log(`Sum: ${firstInteger + secondInteger}`);
+    console.log(`Sum: ${firstDecimal + secondDecimal}`);
+    console.log(`Sum: ${firstString + secondString}\n`);
+    
+    rl.close();
+});
