2479f4f37c590ec46075bc2a91a7a19e5700d8cc
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-01-03 06:54:58 -0300

    add examples

---
+++ 10Days-of-Javascript/day03-try-catch/tutorial/example03.js
@@ -0,0 +1,22 @@
+'use strict'
+
+function throwString() {
+    throw "some excepetion";
+}
+function throwMyError() {
+    throw new Error("My error message!");
+}
+
+try {
+    throwString();
+} 
+catch (e) {
+    console.log(e);
+}
+
+try {
+    throwMyError();
+}
+catch (e) {
+    console.log(e.message);
+}
