9b57198dc3a4e6b17ad16cd0d676fa45746d22cf
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-01-04 09:02:07 -0300

    add inheritance and superclass to examples

--- 10Days-of-Javascript/day04-classes/tutorial/example02.js
+++ 10Days-of-Javascript/day04-classes/tutorial/example02.js
@@ -15,10 +15,17 @@ class Employee {
         this.setJourney = (input) => { journey = input };
     }
 }
+class Manager extends Employee {
+    constructor(n, s, j) {
+        super(n, s, j);
+        this.recognizeEmployee = () => 'Employee recognized with success!';
+    }
+}
 
 let emp01 = new Employee('Thiago', 1500, 4);
 let emp02 = new Employee('Matheus', 3500, 8);
 
+console.log();
 console.log('Employee name: ', emp01.getName());
 emp01.setName('Thiago Luiz');
 console.log('Employee name: ', emp01.getName());
@@ -26,3 +33,9 @@ console.log('Employee name: ', emp01.getName());
 console.log('Employee name: ', emp02.getName());
 emp02.setName('Matheus Cesar');
 console.log('Employee name: ', emp02.getName());
+
+let manager = new Manager('David', 6000, 10);
+
+console.log('Manager name: ', manager.getName());
+console.log(manager.recognizeEmployee());
+console.log();
