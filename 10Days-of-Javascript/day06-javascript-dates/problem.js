7da71c3f5cf5fa50f7d9c74dbef0d3e9393464d1
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-01-08 16:16:31 -0300

    add solution to day06-javascript-dates challenge

---
+++ 10Days-of-Javascript/day06-javascript-dates/problem.js
@@ -0,0 +1,47 @@
+'use strict'
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: console.log
+});
+
+function getDayName(dateString) {
+    let date = new Date(dateString);
+    let dayName;
+
+    switch (date.getDay()) {
+        case 0:
+            dayName = 'Sunday';
+            break;
+        case 1:
+            dayName = 'Monday';
+            break;
+        case 2:
+            dayName = 'Tuesday';
+            break;
+        case 3:
+            dayName = 'Wednesday';
+            break;
+        case 4:
+            dayName = 'Thursday';
+            break;
+        case 5:
+            dayName = 'Friday';
+            break;
+        case 6:
+            dayName = 'Saturday';
+            break;
+    }
+    return dayName;
+}
+
+rl.on('line', input => {
+    console.log(getDayName(input));
+
+    rl.close();
+});
