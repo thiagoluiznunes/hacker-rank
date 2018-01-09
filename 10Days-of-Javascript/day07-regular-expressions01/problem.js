675340a474d3eb75fea8346be2eb65d37ce5bb75
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-01-09 14:01:38 -0300

    init day07 challenge

---
+++ 10Days-of-Javascript/day07-regular-expressions01/problem.js
@@ -0,0 +1,11 @@
+'use strict'
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: console.log
+});
