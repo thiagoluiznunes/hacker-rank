2949fb6483d20b314c1f5f28a151c46c5e8a6a8f
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-01-11 13:36:17 -0300

    update tutorial

--- 10Days-of-Javascript/day07-regular-expressions01/tutorial/example01.js
+++ 10Days-of-Javascript/day07-regular-expressions01/tutorial/example01.js
@@ -8,6 +8,7 @@ const re_ = new RegExp('ab+c');
 
 const test = 'Thiago Luiz Pereira Nunes';
 const test2 = 'Thiago foo barr Nunes';
+const test3 = 'caaaaaaandy';
 // '\'
 console.log(test.match('\a'));
 // Matches to the beginning of the text
@@ -25,3 +26,14 @@ console.log(test.match('.a'));
 console.log(test.match('.ra'));
 // Search matching and do memorize
 console.log(test2.match('(foo) (bar)'));
+// Search matching and do not memorize 
+console.log(test2.match('(?:foo) bar'));
+// Search matching x if x is followed by y x(?=y)
+console.log(test2.match('Thiago(?= foo)'));
+// Search matching x if x isn't followed by y x(?=y)
+console.log(test2.match('a(?!g)'));
+// Search matching by x | y
+console.log(test2.match('Thiago|Nunes'));
+// Search by ocurrences number
+console.log(test3.match('a{3}'));
+console.log(test3.match('a{1,5}'));
