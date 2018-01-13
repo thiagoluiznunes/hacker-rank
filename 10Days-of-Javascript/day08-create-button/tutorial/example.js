3a2accc4c3fdd117b2694e3d7f99a65f219ef7f5
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-01-12 23:30:30 -0300

    update tutorial example

--- 10Days-of-Javascript/day08-create-button/tutorial/example.js
+++ 10Days-of-Javascript/day08-create-button/tutorial/example.js
@@ -1,11 +1,22 @@
 const clickButton = document.createElement('button');
+const clickButton2 = document.createElement('button2');
 
 clickButton.id = 'myButton';
 clickButton.innerHTML = 'Click Me';
 clickButton.style.background = '#4FFF8F';
 
+clickButton2.id = 'myButton2';
+clickButton2.innerHTML = 'Click Me Again';
+clickButton2.style.background = '#4FFF8F';
+clickButton2.style.marginLeft = '5px';
+
 document.body.appendChild(clickButton);
+document.body.appendChild(clickButton2);
 
 clickButton.onclick = function() {
     clickButton.innerHTML = 'New inner';
 }
+
+clickButton2.addEventListener('click', function() {
+    clickButton2.innerHTML = 'You clicked me"';
+});
