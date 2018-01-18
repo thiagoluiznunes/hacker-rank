19aa539f321ed24b351c9170a1f73ae760e95d8d
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-01-18 15:20:34 -0300

    add click event to buttons

--- 10Days-of-Javascript/day09-binary-calculator/problem.js
+++ 10Days-of-Javascript/day09-binary-calculator/problem.js
@@ -1,2 +1,37 @@
 'use strict'
 
+let res = document.getElementById('res');
+let btn0 = document.getElementById('btn0');
+let btn1 = document.getElementById('btn1');
+let btnClr = document.getElementById('btnClr');
+let btnEql = document.getElementById('btnEql');
+let btnSum = document.getElementById('btnSum');
+let btnSub = document.getElementById('btnSub');
+let btnMul = document.getElementById('btnMul');
+let btnDiv = document.getElementById('btnDiv');
+
+btn0.onclick = () => {
+    res.innerHTML += '0';
+}
+btn1.onclick = () => {
+    res.innerHTML += '1';
+}
+btnClr.onclick = () => {
+    res.innerHTML = '';
+}
+btnEql.onclick = () => {
+    res.innerHTML = '';
+    res.innerHTML += 'result';
+}
+btnSum.onclick = () => {
+    res.innerHTML += '+';
+}
+btnSub.onclick = () => {
+    res.innerHTML += '-';
+}
+btnMul.onclick = () => {
+    res.innerHTML += '*';
+}
+btnDiv.onclick = () => {
+    res.innerHTML += '/';
+}
